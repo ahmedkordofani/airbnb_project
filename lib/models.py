d36a3801c4184f9f0544196136852dee37b4961c
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-23 14:23:21 +0100

    Definied peewee database models, create_tables function

---
+++ lib/models.py
@@ -0,0 +1,58 @@
+import peewee, os
+from datetime import datetime
+
+# Connect to relevant database
+if os.environ.get('APP_ENV') == 'dev':
+    db = peewee.PostgresqlDatabase('airbnb-dev')
+elif os.environ.get('APP_ENV') == 'test':
+    db = peewee.PostgresqlDatabase('airbnb-test', host='localhost', port=5432, user='postgres', password='postgres')
+
+
+# define our models
+class User(peewee.Model):
+
+    # fields
+    email = peewee.CharField()
+    password = peewee.CharField()
+
+    # meta class
+    class Meta:
+        database = db
+        table_name = 'users'
+    
+
+class Listing(peewee.Model):
+
+    # fields
+    title = peewee.CharField()
+    description = peewee.TextField()
+    price = peewee.DecimalField()
+    start_date = peewee.DateField(default=datetime.now)
+    end_date = peewee.DateField()
+    owner = peewee.ForeignKeyField(User, backref='listings')
+
+    # meta class
+    class Meta:
+        database = db
+        table_name = 'listings'
+
+
+class Booking(peewee.Model):
+
+    # fields
+    start_date = peewee.DateField()
+    end_date = peewee.DateField()
+    listing = peewee.ForeignKeyField(Listing, backref='bookings')
+    user = peewee.ForeignKeyField(User, backref='bookings')
+    approved = peewee.BooleanField(default=False)
+
+    # meta class
+    class Meta:
+        database = db
+        table_name = 'bookings'
+    
+
+# create tables
+def create_db_tables():
+    with db:
+        db.create_tables([User, Listing, Booking])
