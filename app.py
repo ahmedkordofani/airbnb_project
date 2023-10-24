1c3a56db75de5e936cd3ccb45488918eb76b29b0
Anthony Kamanya <anthony.kamanya@gmail.com>
2023-10-24 12:57:08 +0100

    Added signup route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.models import *
+from lib.validator import Validator
+
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -12,9 +14,32 @@ create_db_tables()
 # Returns the homepage
 # Try it:
 #   ; open http://localhost:5000/index
-@app.route('/index', methods=['GET'])
+
+
+@app.route('/', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+
+@app.route('/signup', methods=['GET'])
+def get_signup_form():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def post_signup_form():
+    vd = Validator()
+    email = request.form["email"]
+    password = request.form["password"]
+    password_repeat = request.form["password-repeat"]
+
+    errors = vd.validate_signup(email, password, password_repeat)
+    existing_user = User.check_email_exists(email)
+    if not existing_user and not errors:
+        return redirect("/")
+
+
+    
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
