2f413c90eb5812dccf508ebb858f275d2e19d993
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-24 11:10:13 +0100

    Wrote login check test

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -5,3 +5,9 @@ def test_email_exists():
 
     assert User.check_email_exists('jdoe@gmail.com')
     assert not User.check_email_exists('example@outlook.com')
+
+def test_login_checks():
+    seed_database()
+
+    assert User.check_login_success('jdoe@gmail.com', 'jdoepassword')
+    assert not User.check_login_success('jdoe@gmail.com', 'wrongpassword')
