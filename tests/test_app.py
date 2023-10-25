9d5c9b7a5f50df9cd5f5bac5810043d528844e9f
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-25 13:04:07 +0100

    Made change to form action

--- tests/test_app.py
+++ tests/test_app.py
@@ -77,6 +77,8 @@ def test_get_login_form(page, test_web_address):
     expect(form).to_have_count(1)
 
 def test_login_success(page, test_web_address):
+    seed_database()
+
     page.goto(f"http://{test_web_address}/login")
 
     email_box = page.locator("#email")
@@ -91,6 +93,8 @@ def test_login_success(page, test_web_address):
     assert page.url == f"http://{test_web_address}/"
 
 def test_login_fail(page, test_web_address):
+    seed_database()
+
     page.goto(f"http://{test_web_address}/login")
 
     email_box = page.locator("#email")
@@ -103,3 +107,39 @@ def test_login_fail(page, test_web_address):
     submit_button.click()
 
     assert page.url == f"http://{test_web_address}/login"
+
+def test_create_listing(page, test_web_address):
+    seed_database()
+
+    page.goto(f"http://{test_web_address}/login")
+
+    email_box = page.locator("#email")
+    email_box.fill("jdoe@gmail.com")
+
+    password_box = page.locator("#password")
+    password_box.fill("jdoepassword")
+
+    submit_button = page.locator("#submit-login")
+    submit_button.click()
+
+    page.goto(f"http://{test_web_address}/spaces/new")
+
+    name_box = page.locator("#name")
+    name_box.fill("Test Listing")
+
+    description_box = page.locator("#description")
+    description_box.fill("Test Description")
+
+    price_box = page.locator("#price")
+    price_box.fill("100")
+
+    start_date_box = page.locator("available-from")
+    start_date_box.fill("24/11/2024")
+
+    end_date_box = page.locator("#available-to")
+    end_date_box.fill("27/11/2024")
+
+    submit_button = page.locator("#submit-listing")
+    submit_button.click()
+
+    assert page.url == f'http://{test_web_address}/'
