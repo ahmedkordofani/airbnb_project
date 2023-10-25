ef73dd15a0a8910902f0017d1e7842b5fad0b567
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-25 10:55:00 +0100

    Added host var to run cmd

--- web/app.py
+++ web/app.py
@@ -76,4 +76,4 @@ def logout():
 # if started in test mode.
 if __name__ == '__main__':
     app.secret_key = os.urandom(24)
-    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
